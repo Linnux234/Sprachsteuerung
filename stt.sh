b4f4bc488fa38ffc81737f2b468cb30a05742e54
Linnux234 <Linnux234@users.noreply.github.com>
2015-09-19 13:45:14 +0200

    Update stt.sh

--- stt.sh
+++ stt.sh
@@ -5,7 +5,7 @@ arecord -d 5 -f dat -t wav | ffmpeg -y -i - -ar 16000 -acodec flac file.flac
 
 echo “Processing…”
 
-wget -q --post-file file.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?client=chromium&lang=de-DE&key=AIzaSyBNEg7UkIx5FTK0sEE8o7hL0DZslI-9ShU" | cut -d: -f6 | cut -c 2- | rev| cut -c 17- | rev > stt.txt
+wget -q --post-file file.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?client=chromium&lang=de-DE&key=[Dein Key]" | cut -d: -f6 | cut -c 2- | rev| cut -c 17- | rev > stt.txt
 
 echo -n “You Said: ”
 cat stt.txt
